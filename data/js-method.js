09a640d2a8be8c3adc583996db0501184a52784f
Siddhant Kumar <siddhuyadav662@gmail.com>
2024-03-28 18:18:33 +0530

    Add Google Analytics consent script to HTML files

--- data/js-method.js
+++ data/js-method.js
@@ -5730,4 +5730,24 @@ function openPopup(popupId) {
   popup.classList.toggle("show"); // Toggle the visibility of the popup
 }
 
+grantButton.addEventListener("click", function() {
+  localStorage.setItem("consentGranted", "true");
+  function gtag() { dataLayer.push(arguments); }
+
+  gtag('consent', 'update', {
+    ad_user_data: 'granted',
+    ad_personalization: 'granted',
+    ad_storage: 'granted',
+    analytics_storage: 'granted'
+  });
+
+  // Load gtag.js script.
+  var gtagScript = document.createElement('script');
+  gtagScript.async = true;
+  gtagScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-G9ERMCCQF1';
+
+  var firstScript = document.getElementsByTagName('script')[0];
+  firstScript.parentNode.insertBefore(gtagScript,firstScript);
+});
+
 
